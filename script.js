97fe5aa4d47e373736e7c94d58e4eb26aa5d6a98
MD Girisim <mdgirisimci-ai@users.noreply.github.com>
2026-09-22 20:16:27 +0300

    Deploy duzeltmesi

--- script.js
+++ script.js
@@ -23,8 +23,7 @@ function phoneStep() {
 
 async function cameraStep() {
     try {
-        const response = await fetch("http://localhost:3000/api/register", {
-            method: "POST",
+        const response = await fetch("/api/register", {            method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
